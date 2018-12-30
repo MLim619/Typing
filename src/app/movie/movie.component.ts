204a5f2bdc43fbfa59a4d0f61dc46a76a9930336
MLim619 <MLim619@gmail.com>
2018-12-30 12:58:19 -0800

    Added typing UI to movie component

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,23 +1,50 @@
 import { ActivatedRoute } from '@angular/router';
 import { DetailedMovie } from './../models/movie';
 import { MovieService } from './../services/movie.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
+
 export class MovieComponent implements OnInit {
 
   movie: DetailedMovie;
-  constructor(private service: MovieService, private route: ActivatedRoute) { }
+
+  @HostListener('document:keydown', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) { 
+    this.evaluateKey(event.key)
+  }
+
+  constructor(private service: MovieService, private route: ActivatedRoute, private el: ElementRef) { }
 
   ngOnInit() {
     this.route.params
       .subscribe(params => {
         this.getMovie(params['id']);
       });
+  }
+  evaluateKey(key: string){
+    let currentLetter = this.el.nativeElement.querySelector('.letter-active');
+    let nextLetter = currentLetter.nextSibling;
+    let previousLetter = currentLetter.previousSibling;
+
+    if (key.length === 1){
+      let newClass = (key === currentLetter.getAttribute('value')) ? 'letter-typed': 'letter-error';
+      currentLetter.classList.replace('letter-active', newClass);
+      
+      nextLetter.classList.add('letter-active');
+    }else{
+      if (key === 'Backspace'){
+        currentLetter.classList.remove('letter-active');
+        previousLetter.classList.remove('letter-typed');
+        previousLetter.classList.remove('letter-error');
+        previousLetter.classList.add('letter-active');
+      }
+    }
   }
   getMovie(id: number){
     this.service.getMovie(id)
